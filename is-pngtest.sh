254b232c3d505a40ccc72130d99b537ed26fb2d3
kyle gendreau <kylegendy@gmail.com>
2022-04-04 14:15:39 -0500

    updated pngtest file

--- is-pngtest.sh
+++ is-pngtest.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
+# create new testing dir
 rm -r libpng/deriv/
 cp -r libpng/libpng-1.6.34/ libpng/deriv/
 
-libpng/deriv/pngtest libpng/large-png-suite/1.png
+# get percent threshold
+# thresh_input="./perc.txt"
+# while IFS= read -r line
+# do
+#   thresh=$line
+# done < "$thresh_input"
 
-gcov libpng/deriv/*.c
+# # pngtest all input
+# for i in $* ; do
+#   libpng/deriv/pngtest libpng/large-png-suite/1.png
+# done
+
+# check if gcov gets bad result
+libpng/deriv/pngtest libpng/large-png-suite/1.png libpng/deriv/
+gcov libpng/deriv/*.c 
 
 exit 0
